123834d686f799f000e5c9eae3a39a524adb8363
RukaiyaCrochet <mustafajambughodagg96@gmail.com>
2026-01-18 00:54:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RCB/",
+  // base: "/RCB/",
   // server: {
   //   allowedHosts: [
-  //     'educated-induction-worldwide-rental.trycloudflare.com',
   //     // You can add more hosts here if needed
   //   ]
   // }
