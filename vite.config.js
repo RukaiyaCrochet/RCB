e769f265dbcbb02330145f86d6679f4759bebf7f
RukaiyaCrochet <mustafajambughodagg96@gmail.com>
2026-01-18 15:58:11 +0530

    Update Vite config file and clean up comments

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/RCB/",
-  // server: {
-  //   allowedHosts: [
-  //     // You can add more hosts here if needed
-  //   ]
-  }
 })
